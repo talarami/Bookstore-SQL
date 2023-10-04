b0402f8906470197b785fa54143c4354babef7e3
talarami <annatalarowskaaa@gmail.com>
2023-10-04 18:05:31 +0200

    scenario

--- scenario.sql
+++ scenario.sql
@@ -1,4 +1,4 @@
 /* A local bookstore that has recently opened has problems with organizing books and their availability.
-Tables are divided into book authors, books and their availability. You can check the numer of 
+Tables are divided into book authors, books and their availability. You are able check the numer of 
 available books, prices of individual items, change book availability, search for books by 
 a specific author, book by title and much more. */
